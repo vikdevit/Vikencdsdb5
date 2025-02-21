c57504391ab806f69d9b6d3014752095b889dabb
vikdevit <v.khatcherian@outlook.fr>
2025-02-21 14:07:30 +0100

    2e commit: ajout de interface.py

---
+++ viken_m2i_cdsdb5_2025_interface.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import requests
+
+st.title("Prédiction de la fleur d'Iris")
+
+sepal_length = st.slider("Longueur du sépale", 4.0, 8.0, 5.1)
+sepal_width = st.slider("Largeur du sépale", 2.0, 4.5, 3.5)
+petal_length = st.slider("Longueur du pétale", 1.0, 7.0, 1.4)
+petal_width = st.slider("Largeur du pétale", 0.1, 2.5, 0.2)
+
+if st.button("Prédire"):
+    input_data = {"features": [sepal_length, sepal_width, petal_length, petal_width]}
+    response = requests.post("http://127.0.0.1:5000/predict", json=input_data)
+
+    if response.status_code == 200:
+        st.write(f"Classe prédite : {response.json()['prediction']}")
+    else:
+        st.write("Erreur lors de la prédiction")
